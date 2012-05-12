edac8ab265c1b296498815b6b311e1218be05fdf
Aliaksandr Simanovich <mnepoh@gmail.com>
2012-05-12 16:04:19 +0300

    Force fail build

--- tests/application/controllers/IndexControllerTest.php
+++ tests/application/controllers/IndexControllerTest.php
@@ -21,6 +21,10 @@ class IndexControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
         $this->assertController($urlParams['controller']);
         $this->assertAction($urlParams['action']);
         $this->assertQueryContentContains("div#welcome h3", "This is your project's main page");
+
+		$this->assertQueryContentContains("div#welcome h1", "asdfasdfadsfadsfasdfasdf");
+
+		asdfsdfadsfad
     }
 
 
