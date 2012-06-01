61a299c583c60577c28cccdf749aba3707cb1e2f
Aliaksandr Simanovich <mnepoh@gmail.com>
2012-06-01 18:27:45 +0300

    Test cpd and md

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -8,10 +8,69 @@ class IndexController extends Zend_Controller_Action
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
-        // action body
-    }
+	public function indexAction()
+	{
+		// action body
+	}
+
+	public function originalTestCode()
+	{
+		$a = 'String';
+		$b = 'String';
+		if (strpos($a, 'Str') !== false)
+		{
+			if (strpos($b, 'Str') !== false)
+			{
+				$this->init();
+			}
+			else
+			{
+				$std = new stdClass();
+				return $std;
+			}
+		}
+	}
+
+	public function copyPastingTestCode()
+	{
+		$a = 'String';
+		$b = 'String';
+		if (strpos($a, 'Str') !== false)
+		{
+			if (strpos($b, 'Str') !== false)
+			{
+				$this->init();
+			}
+			else
+			{
+				$std = new stdClass();
+				return $std;
+			}
+		}
+	}
+
+	public function messyCode()
+	{
+		$a = 'String';
+
+		if (strpos($a, 'Str') !== false)
+		{
+			if (strpos($a, 'Str') !== false)
+			{
+				foreach ($a as $char)
+				{
+					if ($char == 'X')
+					{
+						if (date('Y') != 2020)
+						{
+							$this->init();
+							print 'Messy Code';
+						}
+					}
+				}
+			}
+		}
+	}
 
 
 }
